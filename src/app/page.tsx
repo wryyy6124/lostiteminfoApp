476628170c8a70c5d7d5c40ce69a0eebfa6d95e1
k.tamura <wryyy.6124@gmail.com>
2024-07-05 22:30:01 +0900

    create topPage_layout #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,292 @@
-import Image from "next/image";
+import styles from "./TopPage.module.css";
 
-export default function Home() {
+export default function TopPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <header className={`w-full`}>
+        <div className={`w-full max-w-5xl mx-auto`}>
+          <h1
+            className={`flex flex-wrap flex-col gap-y-1.5 text-center mx-auto py-5 w-fit`}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <span
+              className={`bg-neutral-100 rounded-e-full rounded-l-full text-3xl inline-block place-self-center p-2`}
+            >
+              📦
+            </span>
+            <span className={`block text-sm font-bold`}>
+              おとしものインフォメーション
+            </span>
+          </h1>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      </header>
+      <main className={`w-full`}>
+        <div className={`w-full`}>
+          <h2
+            className={`w-full bg-neutral-100 text-lg font-bold sticky top-0  ${styles.contents_h2}`}
+          >
+            <div className={`w-full max-w-5xl mx-auto p-4`}>
+              🗒️ おとしものリスト
+            </div>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+          <div className={`w-full max-w-5xl mx-auto p-5`}>
+            <div className={`w-full max-w-3xl mx-auto pb-3`}>
+              <h3 className={`text-sm font-bold mb-1.5`}>
+                おとしもの情報フィルター
+              </h3>
+              <input
+                type="text"
+                className={`w-full p-3 rounded-md border-2 border-solid border-gray-200`}
+                name="filter"
+                placeholder="🔍 キーワードを入力してください"
+              />
+            </div>
+            <ol className={`w-full max-w-3xl mx-auto ${styles.item_list}`}>
+              <li
+                className={`flex flex-wrap flex-row items-center w-full max-w-3xl mx-auto py-6 border-b-2 border-b-solid border-gray-200`}
+              >
+                <div
+                  className={`border-2 border-solid text-center max-w-10 ${styles.circle_width} ${styles.is_paid}`}
+                >
+                  <div className={`relative ${styles.circle_height}`}>
+                    <p
+                      className={`flex justify-center items-center absolute top-0 bottom-0 left-0 right-0`}
+                    ></p>
+                  </div>
+                </div>
+                <div className={`flex-auto flex flex-col gap-y-2`}>
+                  <h4 className={`text-sm font-bold text-black`}>
+                    自転車のかぎ
+                  </h4>
+                  <p className={`text-xs text-slate-800`}>
+                    発見日：2024年6月15日
+                  </p>
+                </div>
+                <div className={`flex-none`}>
+                  <button
+                    className={`rounded-md bg-black text-white text-xs px-6 py-3`}
+                  >
+                    詳細
+                  </button>
+                </div>
+              </li>
+              <li
+                className={`flex flex-wrap flex-row items-center w-full max-w-3xl mx-auto py-6 border-b-2 border-b-solid border-gray-200`}
+              >
+                <div
+                  className={`border-2 border-solid text-center max-w-10 ${styles.circle_width} ${styles.is_unpaid}`}
+                >
+                  <div className={`relative ${styles.circle_height}`}>
+                    <p
+                      className={`flex justify-center items-center absolute top-0 bottom-0 left-0 right-0`}
+                    ></p>
+                  </div>
+                </div>
+                <div className={`flex-auto flex flex-col gap-y-2`}>
+                  <h4 className={`text-sm font-bold text-black`}>
+                    自転車のかぎ
+                  </h4>
+                  <p className={`text-xs text-slate-800`}>
+                    発見日：2024年6月15日
+                  </p>
+                </div>
+                <div className={`flex-none`}>
+                  <button
+                    className={`rounded-md bg-black text-white text-xs px-6 py-3`}
+                  >
+                    詳細
+                  </button>
+                </div>
+              </li>
+              <li
+                className={`flex flex-wrap flex-row items-center w-full max-w-3xl mx-auto py-6 border-b-2 border-b-solid border-gray-200`}
+              >
+                <div
+                  className={`border-2 border-solid text-center max-w-10 ${styles.circle_width} ${styles.is_unpaid}`}
+                >
+                  <div className={`relative ${styles.circle_height}`}>
+                    <p
+                      className={`flex justify-center items-center absolute top-0 bottom-0 left-0 right-0`}
+                    ></p>
+                  </div>
+                </div>
+                <div className={`flex-auto flex flex-col gap-y-2`}>
+                  <h4 className={`text-sm font-bold text-black`}>
+                    自転車のかぎ
+                  </h4>
+                  <p className={`text-xs text-slate-800`}>
+                    発見日：2024年6月15日
+                  </p>
+                </div>
+                <div className={`flex-none`}>
+                  <button
+                    className={`rounded-md bg-black text-white text-xs px-6 py-3`}
+                  >
+                    詳細
+                  </button>
+                </div>
+              </li>
+              <li
+                className={`flex flex-wrap flex-row items-center w-full max-w-3xl mx-auto py-6 border-b-2 border-b-solid border-gray-200`}
+              >
+                <div
+                  className={`border-2 border-solid text-center max-w-10 ${styles.circle_width} ${styles.is_unpaid}`}
+                >
+                  <div className={`relative ${styles.circle_height}`}>
+                    <p
+                      className={`flex justify-center items-center absolute top-0 bottom-0 left-0 right-0`}
+                    ></p>
+                  </div>
+                </div>
+                <div className={`flex-auto flex flex-col gap-y-2`}>
+                  <h4 className={`text-sm font-bold text-black`}>
+                    自転車のかぎ
+                  </h4>
+                  <p className={`text-xs text-slate-800`}>
+                    発見日：2024年6月15日
+                  </p>
+                </div>
+                <div className={`flex-none`}>
+                  <button
+                    className={`rounded-md bg-black text-white text-xs px-6 py-3`}
+                  >
+                    詳細
+                  </button>
+                </div>
+              </li>
+              <li
+                className={`flex flex-wrap flex-row items-center w-full max-w-3xl mx-auto py-6 border-b-2 border-b-solid border-gray-200`}
+              >
+                <div
+                  className={`border-2 border-solid text-center max-w-10 ${styles.circle_width} ${styles.is_unpaid}`}
+                >
+                  <div className={`relative ${styles.circle_height}`}>
+                    <p
+                      className={`flex justify-center items-center absolute top-0 bottom-0 left-0 right-0`}
+                    ></p>
+                  </div>
+                </div>
+                <div className={`flex-auto flex flex-col gap-y-2`}>
+                  <h4 className={`text-sm font-bold text-black`}>
+                    自転車のかぎ
+                  </h4>
+                  <p className={`text-xs text-slate-800`}>
+                    発見日：2024年6月15日
+                  </p>
+                </div>
+                <div className={`flex-none`}>
+                  <button
+                    className={`rounded-md bg-black text-white text-xs px-6 py-3`}
+                  >
+                    詳細
+                  </button>
+                </div>
+              </li>
+              <li
+                className={`flex flex-wrap flex-row items-center w-full max-w-3xl mx-auto py-6 border-b-2 border-b-solid border-gray-200`}
+              >
+                <div
+                  className={`border-2 border-solid text-center max-w-10 ${styles.circle_width} ${styles.is_unpaid}`}
+                >
+                  <div className={`relative ${styles.circle_height}`}>
+                    <p
+                      className={`flex justify-center items-center absolute top-0 bottom-0 left-0 right-0`}
+                    ></p>
+                  </div>
+                </div>
+                <div className={`flex-auto flex flex-col gap-y-2`}>
+                  <h4 className={`text-sm font-bold text-black`}>
+                    自転車のかぎ
+                  </h4>
+                  <p className={`text-xs text-slate-800`}>
+                    発見日：2024年6月15日
+                  </p>
+                </div>
+                <div className={`flex-none`}>
+                  <button
+                    className={`rounded-md bg-black text-white text-xs px-6 py-3`}
+                  >
+                    詳細
+                  </button>
+                </div>
+              </li>
+              <li
+                className={`flex flex-wrap flex-row items-center w-full max-w-3xl mx-auto py-6 border-b-2 border-b-solid border-gray-200`}
+              >
+                <div
+                  className={`border-2 border-solid text-center max-w-10 ${styles.circle_width} ${styles.is_unpaid}`}
+                >
+                  <div className={`relative ${styles.circle_height}`}>
+                    <p
+                      className={`flex justify-center items-center absolute top-0 bottom-0 left-0 right-0`}
+                    ></p>
+                  </div>
+                </div>
+                <div className={`flex-auto flex flex-col gap-y-2`}>
+                  <h4 className={`text-sm font-bold text-black`}>
+                    自転車のかぎ
+                  </h4>
+                  <p className={`text-xs text-slate-800`}>
+                    発見日：2024年6月15日
+                  </p>
+                </div>
+                <div className={`flex-none`}>
+                  <button
+                    className={`rounded-md bg-black text-white text-xs px-6 py-3`}
+                  >
+                    詳細
+                  </button>
+                </div>
+              </li>
+              <li
+                className={`flex flex-wrap flex-row items-center w-full max-w-3xl mx-auto py-6 border-b-2 border-b-solid border-gray-200`}
+              >
+                <div
+                  className={`border-2 border-solid text-center max-w-10 ${styles.circle_width} ${styles.is_unpaid}`}
+                >
+                  <div className={`relative ${styles.circle_height}`}>
+                    <p
+                      className={`flex justify-center items-center absolute top-0 bottom-0 left-0 right-0`}
+                    ></p>
+                  </div>
+                </div>
+                <div className={`flex-auto flex flex-col gap-y-2`}>
+                  <h4 className={`text-sm font-bold text-black`}>
+                    自転車のかぎ
+                  </h4>
+                  <p className={`text-xs text-slate-800`}>
+                    発見日：2024年6月15日
+                  </p>
+                </div>
+                <div className={`flex-none`}>
+                  <button
+                    className={`rounded-md bg-black text-white text-xs px-6 py-3`}
+                  >
+                    詳細
+                  </button>
+                </div>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </main>
+      <footer
+        className={`w-full bg-neutral-50 fixed bottom-0 ${styles.footer}`}
+      >
+        <ul className={`w-fit mx-auto flex gap-10 py-2`}>
+          <li className={`text-center`}>
+            <span className={`text-3xl inline-block place-self-center p-2`}>
+              📝
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+            <span className={`block text-sm font-bold`}>アカウント情報</span>
+          </li>
+          <li className={`text-center`}>
+            <span className={`text-3xl inline-block place-self-center p-2`}>
+              🔓
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <span className={`block text-sm font-bold`}>ログオフする</span>
+          </li>
+        </ul>
+      </footer>
+    </>
   );
 }
