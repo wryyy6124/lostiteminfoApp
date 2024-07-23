a555043105e93338836a7051295bfbc65eddeb86
k.tamura <wryyy.6124@gmail.com>
2024-07-23 22:25:18 +0900

    落とし物投稿画面の再プッシュ

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,13 +1,16 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import useAuth from '../useAuth';
-import { createClientComponentClient, Session } from '@supabase/auth-helpers-nextjs';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useState, useEffect } from "react";
+import useAuth from "../useAuth";
+import {
+  createClientComponentClient,
+  Session,
+} from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default function Page() {
   const [session, setSession] = useState<Session | null>(null);
@@ -17,19 +20,21 @@ export default function Page() {
   const [file, setFile] = useState<File | null>(null);
   const [filePreview, setFilePreview] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
-  const [lostitemName, setLostitemName] = useState('');
-  const [findDate, setFindDate] = useState('');
-  const [findTime, setFindTime] = useState('');
-  const [findPlace, setFindPlace] = useState('');
-  const [comment, setComment] = useState('');
-  const [remarksColumn, setRemarksColumn] = useState('');
+  const [lostitemName, setLostitemName] = useState("");
+  const [findDate, setFindDate] = useState("");
+  const [findTime, setFindTime] = useState("");
+  const [findPlace, setFindPlace] = useState("");
+  const [comment, setComment] = useState("");
+  const [remarksColumn, setRemarksColumn] = useState("");
 
   useEffect(() => {
     const getSession = async () => {
-      const { data: { session } } = await supabase.auth.getSession();
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
       setSession(session);
     };
 
@@ -53,72 +58,71 @@ export default function Page() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    setError('');
-    setSuccess('');
+    setError("");
+    setSuccess("");
 
-    let uploadedFilePath = '';
+    let uploadedFilePath = "";
 
     try {
-      let fileURL = '';
+      let fileURL = "";
       if (file) {
         const uniqueFileName = `${Date.now()}_${file.name}`;
         const { data, error } = await supabase.storage
-          .from('post_files')
+          .from("post_files")
           .upload(`public/${uniqueFileName}`, file);
 
         if (error) {
-          throw new Error('画像のアップロードに失敗しました');
+          throw new Error("画像のアップロードに失敗しました");
         }
 
         uploadedFilePath = data.path;
         const timestamp = new Date().toISOString();
-        fileURL = `https://cxhwsktvngsmxnxfhyaj.supabase.co/storage/v1/object/public/post_files/public/${uniqueFileName}?t=${encodeURIComponent(timestamp)}`;
-        console.log('Public URL:', fileURL); // パブリックURLをコンソールに表示
+        fileURL = `https://cxhwsktvngsmxnxfhyaj.supabase.co/storage/v1/object/public/post_files/public/${uniqueFileName}?t=${encodeURIComponent(
+          timestamp
+        )}`;
+        console.log("Public URL:", fileURL); // パブリックURLをコンソールに表示
       }
 
       const { data: dbData, error: dbError } = await supabase
-        .from('post')
+        .from("post")
         .insert([
           {
             lostitem_name: lostitemName,
             find_date: findDate,
-            find_time: findTime || null,  // ここで空の時間をnullとして扱う
+            find_time: findTime || null, // ここで空の時間をnullとして扱う
             find_place: findPlace,
             comment: comment,
             file_url: fileURL,
             resolved: false,
             hidden: false,
             remarks_column: remarksColumn,
-            created_at: new Date().toISOString().split('T')[0],
-            created_by: session?.user?.id || ''
-          }
+            created_at: new Date().toISOString().split("T")[0],
+            created_by: session?.user?.id || "",
+          },
         ]);
 
       if (dbError) {
-        console.error('DB Error:', dbError);
+        console.error("DB Error:", dbError);
 
         // 画像の削除
         if (uploadedFilePath) {
-          await supabase.storage
-            .from('post_files')
-            .remove([uploadedFilePath]);
+          await supabase.storage.from("post_files").remove([uploadedFilePath]);
         }
 
-        throw new Error('データベースへの保存に失敗しました');
+        throw new Error("データベースへの保存に失敗しました");
       }
 
-      setSuccess('投稿が成功しました！');
+      setSuccess("投稿が成功しました！");
 
       // フォームをリセット
-      setLostitemName('');
-      setFindDate('');
-      setFindTime('');
-      setFindPlace('');
-      setComment('');
-      setRemarksColumn('');
+      setLostitemName("");
+      setFindDate("");
+      setFindTime("");
+      setFindPlace("");
+      setComment("");
+      setRemarksColumn("");
       setFile(null);
       setFilePreview(null);
-
     } catch (error: any) {
       setError(error.message);
     } finally {
@@ -128,18 +132,20 @@ export default function Page() {
 
   const signInWithGoogle = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
-      provider: 'google',
+      provider: "google",
     });
     if (error) {
-      console.error('Google Sign-in Error:', error.message);
+      console.error("Google Sign-in Error:", error.message);
     }
   };
 
   return (
     <div className="w-full max-w-3xl mx-auto p-4">
-      <Link className="text-xl mb-4" href={'/'}>To Home Page</Link>
+      <Link className="text-xl mb-4" href={"/"}>
+        To Home Page
+      </Link>
       <div className="text-center mt-10 mb-10">
-        <p>        </p>
+        <p> </p>
       </div>
       {!session ? (
         <button
@@ -155,11 +161,16 @@ export default function Page() {
           </div>
           <form onSubmit={handleSubmit}>
             <div className="mt-10 mb-6">
-              <label htmlFor="lostitemName" className="block text-sm font-bold text-black text-left">落し物:</label>
+              <label
+                htmlFor="lostitemName"
+                className="block text-sm font-bold text-black text-left"
+              >
+                落し物:
+              </label>
               <input
                 type="text"
                 id="lostitemName"
-                placeholder='落し物を入力してください'
+                placeholder="落し物を入力してください"
                 value={lostitemName}
                 onChange={(e) => setLostitemName(e.target.value)}
                 required
@@ -167,7 +178,12 @@ export default function Page() {
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="findDate" className="block text-sm font-bold text-black text-left">発見日:</label>
+              <label
+                htmlFor="findDate"
+                className="block text-sm font-bold text-black text-left"
+              >
+                発見日:
+              </label>
               <input
                 type="date"
                 id="findDate"
@@ -178,7 +194,12 @@ export default function Page() {
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="findTime" className="block text-sm font-bold text-black text-left">発見時間:</label>
+              <label
+                htmlFor="findTime"
+                className="block text-sm font-bold text-black text-left"
+              >
+                発見時間:
+              </label>
               <input
                 type="time"
                 id="findTime"
@@ -188,11 +209,16 @@ export default function Page() {
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="findPlace" className="block text-sm font-bold text-black text-left">発見場所:</label>
+              <label
+                htmlFor="findPlace"
+                className="block text-sm font-bold text-black text-left"
+              >
+                発見場所:
+              </label>
               <input
                 type="text"
                 id="findPlace"
-                placeholder='発見場所を入力してください'
+                placeholder="発見場所を入力してください"
                 value={findPlace}
                 onChange={(e) => setFindPlace(e.target.value)}
                 required
@@ -200,27 +226,42 @@ export default function Page() {
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="comment" className="block text-sm font-bold text-black text-left">コメント:</label>
+              <label
+                htmlFor="comment"
+                className="block text-sm font-bold text-black text-left"
+              >
+                コメント:
+              </label>
               <textarea
                 id="comment"
-                placeholder='コメントを入力してください'
+                placeholder="コメントを入力してください"
                 value={comment}
                 onChange={(e) => setComment(e.target.value)}
                 className="mt-1 p-2 border border-gray-300 rounded-md w-full"
               ></textarea>
             </div>
             <div className="mb-6">
-              <label htmlFor="remarksColumn" className="block text-sm font-bold text-black text-left">備考欄:</label>
+              <label
+                htmlFor="remarksColumn"
+                className="block text-sm font-bold text-black text-left"
+              >
+                備考欄:
+              </label>
               <textarea
                 id="remarksColumn"
-                placeholder='その他の情報を入力してください'
+                placeholder="その他の情報を入力してください"
                 value={remarksColumn}
                 onChange={(e) => setRemarksColumn(e.target.value)}
                 className="mt-1 p-2 border border-gray-300 rounded-md w-full"
               ></textarea>
             </div>
             <div className="mb-6">
-              <label htmlFor="file" className="block text-sm font-bold text-black mb-2 text-left">画像ファイルをアップロード:</label>
+              <label
+                htmlFor="file"
+                className="block text-sm font-bold text-black mb-2 text-left"
+              >
+                画像ファイルをアップロード:
+              </label>
               <input
                 type="file"
                 id="file"
@@ -230,20 +271,34 @@ export default function Page() {
               />
               {filePreview && (
                 <div className="mt-4">
-                  <p className="text-sm font-bold text-black text-left">プレビュー:</p>
-                  <Image src={filePreview} alt="プレビュー" width={300} height={300} className="mt-2 max-w-full h-auto" />
+                  <p className="text-sm font-bold text-black text-left">
+                    プレビュー:
+                  </p>
+                  <Image
+                    src={filePreview}
+                    alt="プレビュー"
+                    width={300}
+                    height={300}
+                    className="mt-2 max-w-full h-auto"
+                  />
                 </div>
               )}
-              <p className="block mt-3 text-xs text-red-500 text-left">不適切な画像のアップロードは禁止されています</p>
-              <p className="block mt-3 text-xs text-black text-left">ファイル名は半角にしてください</p>
+              <p className="block mt-3 text-xs text-red-500 text-left">
+                不適切な画像のアップロードは禁止されています
+              </p>
+              <p className="block mt-3 text-xs text-black text-left">
+                ファイル名は半角にしてください
+              </p>
             </div>
             <div className="flex align-items: center justify-center">
               <button
                 type="submit"
                 disabled={loading}
-                className={`p-2 w-1/2 mt-12 bg-black text-white rounded-md mx-auto ${loading ? 'bg-gray-700' : 'hover:bg-gray-900'}`}
+                className={`p-2 w-1/2 mt-12 bg-black text-white rounded-md mx-auto ${
+                  loading ? "bg-gray-700" : "hover:bg-gray-900"
+                }`}
               >
-                {loading ? '投稿中...' : '投稿する'}
+                {loading ? "投稿中..." : "投稿する"}
               </button>
             </div>
           </form>
@@ -252,7 +307,7 @@ export default function Page() {
         </>
       )}
       <div className="text-center mt-10 mb-10">
-        <p>        </p>
+        <p> </p>
       </div>
     </div>
   );
