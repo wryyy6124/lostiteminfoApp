a226cb9f3ab8727a9b4af671ddd474e0fa2b84be
k.tamura <wryyy.6124@gmail.com>
2024-07-23 22:31:13 +0900

    mainブランチへ落とし物投稿画面の再々プッシュ

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -10,9 +10,11 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 
+import styles from "./PostPage.module.css";
+
 export const dynamic = "force-dynamic";
 
-export default function Page() {
+export default function PostPage() {
   const [session, setSession] = useState<Session | null>(null);
   const router = useRouter();
   const supabase = createClientComponentClient();
@@ -35,6 +37,7 @@ export default function Page() {
       const {
         data: { session },
       } = await supabase.auth.getSession();
+
       setSession(session);
     };
 
@@ -45,18 +48,23 @@ export default function Page() {
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0];
+
     if (selectedFile) {
       setFile(selectedFile);
+
       const reader = new FileReader();
+
       reader.onloadend = () => {
         setFilePreview(reader.result as string);
       };
+
       reader.readAsDataURL(selectedFile);
     }
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
     setLoading(true);
     setError("");
     setSuccess("");
@@ -65,8 +73,10 @@ export default function Page() {
 
     try {
       let fileURL = "";
+
       if (file) {
         const uniqueFileName = `${Date.now()}_${file.name}`;
+
         const { data, error } = await supabase.storage
           .from("post_files")
           .upload(`public/${uniqueFileName}`, file);
@@ -76,10 +86,13 @@ export default function Page() {
         }
 
         uploadedFilePath = data.path;
+
         const timestamp = new Date().toISOString();
+
         fileURL = `https://cxhwsktvngsmxnxfhyaj.supabase.co/storage/v1/object/public/post_files/public/${uniqueFileName}?t=${encodeURIComponent(
           timestamp
         )}`;
+
         console.log("Public URL:", fileURL); // パブリックURLをコンソールに表示
       }
 
@@ -130,185 +143,212 @@ export default function Page() {
     }
   };
 
-  const signInWithGoogle = async () => {
-    const { error } = await supabase.auth.signInWithOAuth({
-      provider: "google",
-    });
-    if (error) {
-      console.error("Google Sign-in Error:", error.message);
-    }
-  };
-
   return (
-    <div className="w-full max-w-3xl mx-auto p-4">
-      <Link className="text-xl mb-4" href={"/"}>
-        To Home Page
-      </Link>
-      <div className="text-center mt-10 mb-10">
-        <p> </p>
-      </div>
-      {!session ? (
-        <button
-          onClick={signInWithGoogle}
-          className="mt-8 px-10 py-6 bg-gray-600 text-white text-xl font-semibold rounded-lg hover:bg-gray-700 transition-all duration-300 transform hover:scale-105 shadow-lg"
-        >
-          Googleでサインイン
-        </button>
-      ) : (
-        <>
-          <div>
-            <p>投稿者 ID: {session.user?.id}</p>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div className="mt-10 mb-6">
-              <label
-                htmlFor="lostitemName"
-                className="block text-sm font-bold text-black text-left"
-              >
-                落し物:
-              </label>
-              <input
-                type="text"
-                id="lostitemName"
-                placeholder="落し物を入力してください"
-                value={lostitemName}
-                onChange={(e) => setLostitemName(e.target.value)}
-                required
-                className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-              />
+    <>
+      {/* ヘッダー */}
+      <header className={`w-full`}>
+        <div className={`w-full max-w-5xl mx-auto`}>
+          <h1
+            className={`flex flex-wrap flex-col gap-y-1.5 text-center mx-auto py-5 w-fit`}
+          >
+            <span
+              className={`bg-neutral-100 rounded-e-full rounded-l-full text-3xl inline-block place-self-center p-2`}
+            >
+              📦
+            </span>
+            <span className={`block text-sm font-bold`}>
+              おとしものインフォメーション
+            </span>
+          </h1>
+        </div>
+      </header>
+
+      {/* メインコンテンツ */}
+      <main className={`w-full pb-24`}>
+        <div className={`w-full`}>
+          <h2
+            className={`w-full bg-neutral-100 text-lg font-bold text-red-500 z-10 sticky top-0 ${styles.contents_h2}`}
+          >
+            <div className={`w-full max-w-5xl mx-auto p-4`}>
+              📝 新規スレッドを投稿する
             </div>
-            <div className="mb-6">
-              <label
-                htmlFor="findDate"
-                className="block text-sm font-bold text-black text-left"
+          </h2>
+          <div className={`w-full max-w-5xl mx-auto pt-2 pb-10 px-5 mt-5`}>
+            <div className={`w-full`}>
+              <div
+                className={`w-full max-w-3xl mx-auto mb-10 px-5 text-center text-lg font-bold text-teal-400`}
               >
-                発見日:
-              </label>
-              <input
-                type="date"
-                id="findDate"
-                value={findDate}
-                onChange={(e) => setFindDate(e.target.value)}
-                required
-                className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="findTime"
-                className="block text-sm font-bold text-black text-left"
-              >
-                発見時間:
-              </label>
-              <input
-                type="time"
-                id="findTime"
-                value={findTime}
-                onChange={(e) => setFindTime(e.target.value)}
-                className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="findPlace"
-                className="block text-sm font-bold text-black text-left"
-              >
-                発見場所:
-              </label>
-              <input
-                type="text"
-                id="findPlace"
-                placeholder="発見場所を入力してください"
-                value={findPlace}
-                onChange={(e) => setFindPlace(e.target.value)}
-                required
-                className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="comment"
-                className="block text-sm font-bold text-black text-left"
-              >
-                コメント:
-              </label>
-              <textarea
-                id="comment"
-                placeholder="コメントを入力してください"
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-              ></textarea>
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="remarksColumn"
-                className="block text-sm font-bold text-black text-left"
-              >
-                備考欄:
-              </label>
-              <textarea
-                id="remarksColumn"
-                placeholder="その他の情報を入力してください"
-                value={remarksColumn}
-                onChange={(e) => setRemarksColumn(e.target.value)}
-                className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-              ></textarea>
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="file"
-                className="block text-sm font-bold text-black mb-2 text-left"
-              >
-                画像ファイルをアップロード:
-              </label>
-              <input
-                type="file"
-                id="file"
-                accept="image/*"
-                onChange={handleFileChange}
-                className="mt-4"
-              />
-              {filePreview && (
-                <div className="mt-4">
-                  <p className="text-sm font-bold text-black text-left">
-                    プレビュー:
-                  </p>
-                  <Image
-                    src={filePreview}
-                    alt="プレビュー"
-                    width={300}
-                    height={300}
-                    className="mt-2 max-w-full h-auto"
+                <p>投稿者 ID: {session?.user?.id}</p>
+              </div>
+
+              <form onSubmit={handleSubmit}>
+                <div className={`mb-6`}>
+                  <label
+                    htmlFor="lostitemName"
+                    className={`block text-lg font-bold text-black text-left mb-2`}
+                  >
+                    落し物:
+                  </label>
+                  <input
+                    type="text"
+                    id="lostitemName"
+                    placeholder="落し物を入力してください"
+                    value={lostitemName}
+                    onChange={(e) => setLostitemName(e.target.value)}
+                    required
+                    className={`mt-1 p-2 border border-gray-300 rounded-md w-full`}
                   />
                 </div>
-              )}
-              <p className="block mt-3 text-xs text-red-500 text-left">
-                不適切な画像のアップロードは禁止されています
-              </p>
-              <p className="block mt-3 text-xs text-black text-left">
-                ファイル名は半角にしてください
-              </p>
+                <div className={`flex flex-row gap-10 mb-6`}>
+                  <div className={`flex-auto`}>
+                    <label
+                      htmlFor="findDate"
+                      className={`block text-lg font-bold text-black text-left mb-2`}
+                    >
+                      発見日:
+                    </label>
+                    <input
+                      type="date"
+                      id="findDate"
+                      value={findDate}
+                      onChange={(e) => setFindDate(e.target.value)}
+                      required
+                      className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+                    />
+                  </div>
+                  <div className={`flex-auto`}>
+                    <label
+                      htmlFor="findTime"
+                      className={`block text-lg font-bold text-black text-left mb-2`}
+                    >
+                      発見時間:
+                    </label>
+                    <input
+                      type="time"
+                      id="findTime"
+                      value={findTime}
+                      onChange={(e) => setFindTime(e.target.value)}
+                      className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+                    />
+                  </div>
+                </div>
+                <div className={`mb-6`}>
+                  <label
+                    htmlFor="findPlace"
+                    className={`block text-lg font-bold text-black text-left mb-2`}
+                  >
+                    発見場所:
+                  </label>
+                  <input
+                    type="text"
+                    id="findPlace"
+                    placeholder="発見場所を入力してください"
+                    value={findPlace}
+                    onChange={(e) => setFindPlace(e.target.value)}
+                    required
+                    className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+                  />
+                </div>
+                <div className={`mb-6`}>
+                  <label
+                    htmlFor="comment"
+                    className={`block text-lg font-bold text-black text-left mb-2`}
+                  >
+                    コメント:
+                  </label>
+                  <textarea
+                    id="comment"
+                    placeholder="コメントを入力してください"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    className={`mt-1 p-2 border border-gray-300 rounded-md w-full min-h-40 resize-y`}
+                  ></textarea>
+                </div>
+                <div className={`mb-6`}>
+                  <label
+                    htmlFor="remarksColumn"
+                    className={`block text-lg font-bold text-black text-left mb-2`}
+                  >
+                    備考欄:
+                  </label>
+                  <textarea
+                    id="remarksColumn"
+                    placeholder="その他の情報を入力してください"
+                    value={remarksColumn}
+                    onChange={(e) => setRemarksColumn(e.target.value)}
+                    className={`mt-1 p-2 border border-gray-300 rounded-md w-full min-h-40 resize-y`}
+                  ></textarea>
+                </div>
+                <div className={`bg-amber-50 p-5 rounded-lg`}>
+                  <label
+                    htmlFor="file"
+                    className={`block text-lg font-bold text-black mb-2 text-left mb-2`}
+                  >
+                    画像ファイルをアップロード:
+                  </label>
+                  <input
+                    type="file"
+                    id="file"
+                    accept="image/*"
+                    onChange={handleFileChange}
+                    className={`mt-4`}
+                  />
+                  {filePreview && (
+                    <div className={`mt-4`}>
+                      <p
+                        className={`text-sm font-bold text-black text-left mb-2`}
+                      >
+                        プレビュー:
+                      </p>
+                      <Image
+                        src={filePreview}
+                        alt="プレビュー"
+                        width={300}
+                        height={300}
+                        className={`mt-2 max-w-full h-auto`}
+                      />
+                    </div>
+                  )}
+                  <p className={`block mt-3 text-xs text-red-500 text-left`}>
+                    不適切な画像のアップロードは禁止されています
+                  </p>
+                  <p className={`block mt-3 text-xs text-black text-left`}>
+                    ファイル名は半角にしてください
+                  </p>
+                </div>
+                <div className={`flex align-items: center justify-center`}>
+                  <button
+                    type="submit"
+                    disabled={loading}
+                    className={`p-6 w-1/2 mt-12 bg-black text-white text-lg font-semibold rounded-lg mx-auto ${
+                      loading ? "bg-gray-700" : "hover:bg-gray-900"
+                    }`}
+                  >
+                    {loading ? "投稿中..." : "投稿する"}
+                  </button>
+                </div>
+              </form>
+
+              {error && <p className={`text-red-500 mt-10`}>{error}</p>}
+              {success && <p className={`text-green-500 mt-10`}>{success}</p>}
             </div>
-            <div className="flex align-items: center justify-center">
-              <button
-                type="submit"
-                disabled={loading}
-                className={`p-2 w-1/2 mt-12 bg-black text-white rounded-md mx-auto ${
-                  loading ? "bg-gray-700" : "hover:bg-gray-900"
-                }`}
-              >
-                {loading ? "投稿中..." : "投稿する"}
-              </button>
-            </div>
-          </form>
-          {error && <p className="text-red-500 mt-10">{error}</p>}
-          {success && <p className="text-green-500 mt-10">{success}</p>}
-        </>
-      )}
-      <div className="text-center mt-10 mb-10">
-        <p> </p>
-      </div>
-    </div>
+          </div>
+        </div>
+      </main>
+
+      {/* フッター */}
+      <footer
+        className={`w-full bg-neutral-50 fixed bottom-0 ${styles.footer}`}
+      >
+        <ul className={`w-fit mx-auto flex gap-10 py-2`}>
+          <Link href={`../../account`}>
+            <span className={`text-3xl inline-block place-self-center p-2`}>
+              🚪
+            </span>
+            <span className={`block text-sm font-bold text-center`}>戻る</span>
+          </Link>
+        </ul>
+      </footer>
+    </>
   );
 }
